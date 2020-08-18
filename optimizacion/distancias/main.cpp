b64ffccc9a8bee72b50c2e95b545d821d25c56ad
LuisDHuante <69726163+LuisDHuante@users.noreply.github.com>
2020-08-18 18:49:28 -0500

    Update main.cpp

--- optimizacion/distancias/main.cpp
+++ optimizacion/distancias/main.cpp
@@ -1,12 +1,10 @@
-//Lista con nombres []
+ char nombres[12] = { "Luis Huante", "Angel Flores", "Roberto Rangel", "Marco Polo", "Homero Simpson", "James Bond", "Clint Eastwood", "Marty McFly", "Porfifrio Díaz", "Andres Lopez"
+                  "Miguel Hidalgo", "Xavier Lopez"}; 
+cout << "Inserta el nombre que quieras comparar\n";
+cin >> nombre;
 
-cout << "Inserta el nombre que quieras comparar";
-cin >> nombre
 
+  for n in nombres;  
+    nombre.comparar(n);
 
-  for n in Lista con nombres  
-    nombre.comparar(n)
-
-    if (nombre =/ n)
-      remover de lista
-
+  
