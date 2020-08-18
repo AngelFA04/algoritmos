9178113e7ccf490b71533fee25cece89a1cb5678
LuisDHuante <69726163+LuisDHuante@users.noreply.github.com>
2020-08-18 18:24:59 -0500

    Update main.cpp

---
+++ optimizacion/distancias/main.cpp
@@ -0,0 +1,12 @@
+//Lista con nombres []
+
+cout << "Inserta el nombre que quieras comparar";
+cin >> nombre
+
+
+  for n in Lista con nombres  
+    nombre.comparar(n)
+
+    if (nombre =/ n)
+      remover de lista
+
