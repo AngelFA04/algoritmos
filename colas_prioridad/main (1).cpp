7c748b90e9da70d6fec0aee8ea3784af8c4e627e
LuisDHuante <69726163+LuisDHuante@users.noreply.github.com>
2020-08-19 10:47:50 -0500

    Update main (1).cpp

--- colas_prioridad/main (1).cpp
+++ colas_prioridad/main (1).cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    int nNodos, respuesta;
+    int numNodos, respuesta;
     int **matrizAdj;
     bool correcto = true;
     bool correcto2 = true;
@@ -12,8 +12,9 @@ int main() {
     //Empezamos a definir el grafo, comenzando por el número de nodos
     cout << "Escribe el numero de nodos de tu grafo: ";
     cin >> numNodos;
-    int matrizAdjT[nNodos][nNodos];
-
+    int matrizAdjT[numNodos][numNodos];
+    
+    //¿?
     while(correcto){
     for(int i=0; i<numNodos; i++){
         for(int x=0; x<numNodos; x++){
@@ -22,18 +23,18 @@ int main() {
 
         }
     }
-    cout<<"\n";
-    cout<<"La matriz almacenada"<<endl;
+    //Imprimimos la matriz resultante
+    cout<<"La matriz almacenada es"<<endl;
       for(int i=0; i<numNodos; i++){
         for(int x=0; x<numNodos; x++){
             cout << matrizAdjT[i][x] <<" ";
 
         }
-        cout <<"\n";
     }
+    //Confirmamos la matriz
     cout<<"\n";
     do{
-    cout<<"1. SI\n2. NO\n¿Es correcta la matriz? ";
+    cout<<"1. Si\n2. No\nIndica si la matriz es correcta ";
     cin >> respuesta;
     cout<<"\n";
     if(respuesta == 1 ){
@@ -43,7 +44,7 @@ int main() {
         correcto = true;
         correcto2 = false;
         }else{
-            cout<< "Esa opcion no es valida! "<<endl;
+            cout<< "Esa opcion no es valida "<<endl;
             correcto2 = true;
             }
         }while(correcto2);
@@ -59,6 +60,7 @@ int main() {
             matrizAdj[i][j] = matrizAdjT[i][j];
         }
     }
+    //Pasamos a aplicar Dijkstra y calcular la distancia más corta
     Dijkstra d(numNodos, matrizAdj);
     for(int i = 0; i < numNodos; i++){
         d.calcularDC(i);
